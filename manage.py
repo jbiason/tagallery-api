8b3808b6f0a40bfda752fbc75edd171c35e01651
Julio Biason <julioandre@cwi.com.br>
2014-05-02 14:23:41 -0300

    added the management tool

---
+++ manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""Tool for managing Tagallery."""
+
+import logging
+
+from flask.ext.script import Manager
+
+from .api.server import app
+
+manager = Manager(app)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    app.config['DEBUG'] = True
+    manager.run()
