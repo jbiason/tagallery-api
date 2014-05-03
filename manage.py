d4d07ccd469c7dcf077ae554df88de2ee93d947b
Julio Biason <julio.biason@gmail.com>
2014-05-03 08:17:51 -0300

    adding admins

--- manage.py
+++ manage.py
@@ -20,6 +20,32 @@ def show_config():
         print '{key}: {value}'.format(
             key=key, value=app.config[key])
 
+
+# add a new administrator
+@manager.command
+@manager.option('-u', '--user', dest='user')
+@manager.option('-p', '--password', dest='password')
+def adduser(user, password):
+    """Add a new administrator."""
+    if not user or not password:
+        print 'Username and password are required'
+        return
+
+    import crypt
+    from api.server import User
+
+    from pony.orm import db_session
+    from pony.orm import commit
+
+    salt = user[0] + user[-1]
+    cyphered = crypt.crypt(password, salt)
+
+    with db_session:
+        User(login=user, password=cyphered)
+        commit()
+
+
+# running
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     app.config['DEBUG'] = True
