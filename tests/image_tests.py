4ce0c28a9d6667540e9dc1aee13ec86f850d9cfd
Julio Biason <julioandre@cwi.com.br>
2014-05-07 14:28:50 -0300

    added the tests for the tags

--- tests/image_tests.py
+++ tests/image_tests.py
@@ -114,10 +114,54 @@ class ImageTests(BaseImage):
         self.assertJSONOk(rv)
 
         data = json.loads(rv.data)
-        self.assertTrue(len(data['images']) == 2)
+        self.assertEqual(len(data['images']), 2)
 
         # because riker1 was created last, it should be the first in the
         # results
-        self.assertTrue(data['images'][0]['filename'] == 'riker1.gif')
-        self.assertTrue(data['images'][1]['filename'] == 'riker.gif')
+        self.assertEqual(data['images'][0]['filename'], 'riker1.gif')
+        self.assertEqual(data['images'][1]['filename'], 'riker.gif')
+        return
+
+    def test_index_single_tag(self):
+        """Request the index with a single tag."""
+        self.add_to_images('riker.gif', tags=['gif', 'riker'])
+        self.add_to_images('riker.gif', tags=['gif', 'copy'],
+                           target_filename='riker1.gif')
+        rv = self.get('/image/?tags=copy')
+        self.assertJSONOk(rv)
+
+        data = json.loads(rv.data)
+        self.assertEqual(len(data['images']), 1)
+        self.assertEqual(data['images'][0]['filename'], 'riker1.gif')
+        return
+
+    def test_index_single_tag_multiple_returns(self):
+        """Request the index with a single tag, returning multiple values."""
+        self.add_to_images('riker.gif', tags=['gif', 'riker'])
+        self.add_to_images('riker.gif', tags=['gif', 'copy'],
+                           target_filename='riker1.gif')
+        rv = self.get('/image/?tags=gif')
+        self.assertJSONOk(rv)
+
+        data = json.loads(rv.data)
+        self.assertEqual(len(data['images']), 1)
+        self.assertEqual(data['images'][0]['filename'], 'riker1.gif')
+        self.assertEqual(data['images'][1]['filename'], 'riker.gif')
+        return
+
+    def test_index_multile_tags(self):
+        """Get the index with images from multiple tags."""
+        self.add_to_images('riker.gif', tags=['gif', 'riker', 'star trek'])
+        self.add_to_images('riker.gif', tags=['gif', 'trumpet'],
+                           target_filename='trumpet.gif')
+        self.add_to_images('riker.gif', tags=['gif', 'star trek', 'silly'],
+                           target_filename='silly.gif')
+
+        rv = self.get('/image/?tags=gif,star+trek')
+        self.assertJSONOk(rv)
+
+        data = json.loads(rv.data)
+        self.assertEqual(len(data['images']), 2)
+        self.assertEqual(data['images'][0]['filename'], 'silly.gif')
+        self.assertEqual(data['images'][1]['filename'], 'riker.gif')
         return
